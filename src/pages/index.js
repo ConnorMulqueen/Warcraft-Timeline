797abf69350692ebc5900c1e65d6de6abc840915
Connor Mulqueen <connor.mulqueen@simplybusiness.com>
2020-12-07 14:57:51 -0500

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,252 @@
-import * as React from "react"
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Timeline from '@material-ui/lab/Timeline';
+import Typography from '@material-ui/core/Typography';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
+import TimelineItemComponent from './timelineItemComponent';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-  listStyleType: "none",
-}
-const listItemStyles = {
-  marginBottom: 12,
-  fontWeight: "300",
-  letterSpacing: 1,
-}
-const linkStyles = {
-  color: "#8954A8",
-}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '6px 16px',
+  },
+  secondaryTail: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  media: {
+    height: 300,
+  },
+}));
 
-// data
-const links = [
-  {
-    text: "Documentation",
-    url: "https://www.gatsbyjs.com/docs/",
-  },
-  {
-    text: "Tutorials",
-    url: "https://www.gatsbyjs.com/tutorial/",
-  },
-  {
-    text: "Guides",
-    url: "https://www.gatsbyjs.com/tutorial/",
-  },
-  {
-    text: "API Reference",
-    url: "https://www.gatsbyjs.com/docs/api-reference/",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-  },
-  {
-    text: "Cheat Sheet",
-    url: "https://www.gatsbyjs.com/docs/cheat-sheet/",
-  },
-]
+export default function CustomizedTimeline() {
+  const classes = useStyles();
 
-// markup
-const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site!</span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li style={listItemStyles}>
-            <a
-              style={linkStyles}
-              href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-            >
-              {link.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2C13.3132 2 14.6136 2.25866 15.8268 2.7612C17.0401 3.26375 18.1425 4.00035 19.0711 4.92893C19.9997 5.85752 20.7362 6.95991 21.2388 8.17317C21.7413 9.38642 22 10.6868 22 12C22 14.6522 20.9464 17.1957 19.0711 19.0711C17.1957 20.9464 14.6522 22 12 22C10.6868 22 9.38642 21.7413 8.17317 21.2388C6.95991 20.7362 5.85752 19.9997 4.92893 19.0711C3.05357 17.1957 2 14.6522 2 12C2 9.34784 3.05357 6.8043 4.92893 4.92893C6.8043 3.05357 9.34784 2 12 2ZM12 4C8.27 4 5.14 6.55 4.25 10L14 19.75C17.45 18.86 20 15.73 20 12H14.75V13.5H18.2C17.71 15.54 16.24 17.19 14.31 17.94L6.06 9.69C7 7.31 9.3 5.63 12 5.63C14.13 5.63 16 6.67 17.18 8.28L18.41 7.22C16.95 5.26 14.63 4 12 4ZM4 12C4 14.1217 4.84285 16.1566 6.34315 17.6569C7.84344 19.1571 9.87827 20 12 20C12.04 20 12.09 20 4 12Z' fill='%23663399'/%3E%3C/svg%3E%0A"
-      />
-    </main>
-  )
-}
+    <div>
+      <AppBar>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Warcraft Timeline
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="h3" align="center">
+        Early Azeroth History
+      </Typography>
+        <Timeline align="alternate">
+          <TimelineItemComponent
+            year="-150,000"
+            title="World of Warcraft: Chronicle Volume 1"
+            image="/../../static/World_of_Warcraft_Chronicle_Volume_1.jpg"
+            type="Illustrated Novel"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="War of the Ancients Trilogy"
+            image="/../../static/War_of_the_ancients_Trilogy_book_1.jpg"
+            type="Novel"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-110,000"
+            title="World of Warcraft: Chronicle Volume 2"
+            image="/../../static/World_of_Warcraft_Chronicle_Volume_2.jpg"
+            type="Illustrated Novel"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="A Warrior Made"
+            image="/../../static/Warcraft_Legends_Volume_4.jpg"
+            type="Manga"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="Rise of the Horde"
+            image="/../../static/rise_of_the_horde.jpg"
+            type="Novel"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="Family Values"
+            image="/../../static/Warcraft_Legends_Volume_2.jpg"
+            type="Manga"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="Unbroken"
+            image="/../../static/unbroken.jpg"
+            type="Short Story"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="The First Guardian"
+            image="/../../static/Warcraft_Legends_Volume_5.jpg"
+            type="Manga"
+            link="somethingsomething">
+          </TimelineItemComponent>
+          <TimelineItemComponent
+            year="-125,000"
+            title="The Last Guardian"
+            image="/../../static/The_Last_Guardian.jpg"
+            type="Novel"
+            link="somethingsomething">
+          </TimelineItemComponent>
+        </Timeline>
 
-export default IndexPage
+      <Typography variant="h3" align="center">
+          Warcraft: Orcs and Humans
+      </Typography>
+        <Timeline align="alternate">
+          <TimelineItemComponent
+            year="-150,000"
+            title="Warcraft: Orcs and Humans"
+            image="/../../static/Warcraft_Orcs_and_Humans.jpg"
+            type="Game"
+            link="somethingsomething">
+          </TimelineItemComponent>
+        </Timeline>
+
+      <Typography variant="h3" align="center">
+        Warcraft II: Tides of Darkness
+      </Typography>
+        <Timeline align="alternate">
+          <TimelineItemComponent
+              year="-150,000"
+              title="Warcraft II: Tides of Darkness"
+              image="/../../static/"
+              type="Game"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Tides of Darkness"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+        </Timeline>
+
+
+      <Typography variant="h3" align="center">
+        Warcraft II: Beyond the Dark Portal
+      </Typography>
+        <Timeline align="alternate">
+          <TimelineItemComponent
+              year="-150,000"
+              title="Warcraft II: Beyond the Dark Portal"
+              image="/../../static/"
+              type="Game"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Beyond the Dark Portal"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Day of the Dragon"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Road to Damnation"
+              image="/../../static/"
+              type="Short Story"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Lord of the Clans"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Of Blood and Honor"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+        </Timeline>
+
+      <Typography variant="h3" align="center">
+        Warcraft III: Reign of Chaos
+      </Typography>
+        <Timeline align="alternate">
+        <TimelineItemComponent
+              year="-150,000"
+              title="Warcraft III: Reign of Chaos"
+              image="/../../static/"
+              type="Game"
+              link="somethingsomething">
+            </TimelineItemComponent>
+        </Timeline>
+
+      <Typography variant="h3" align="center">
+       Warcraft III: The Frozen Throne
+      </Typography>
+        <Timeline align="alternate">
+        <TimelineItemComponent
+              year="-150,000"
+              title="Warcraft III: The Frozen Throne"
+              image="/../../static/"
+              type="Game"
+              link="somethingsomething">
+            </TimelineItemComponent>
+            <TimelineItemComponent
+              year="-150,000"
+              title="Arthas: Rise of the Lich King"
+              image="/../../static/"
+              type="Novel"
+              link="somethingsomething">
+            </TimelineItemComponent>
+        </Timeline>
+
+      <Typography variant="h3" align="center">
+       Pre-WoW todo
+      </Typography>
+      <Typography variant="h3" align="center">
+       World of Warcraft
+      </Typography>
+      <Typography variant="h3" align="center">
+        The Burning Crusade
+      </Typography>
+      <Typography variant="h3" align="center">
+        Wrath of the Lich King
+      </Typography>
+      <Typography variant="h3" align="center">
+        Cataclysm
+      </Typography>
+      <Typography variant="h3" align="center">
+        Mists of Pandaria
+      </Typography>
+      <Typography variant="h3" align="center">
+        Warlords of Draenor
+      </Typography>
+      <Typography variant="h3" align="center">
+        Legion
+      </Typography>
+      <Typography variant="h3" align="center">
+        Battle for Azeroth
+      </Typography>
+      <Typography variant="h3" align="center">
+        Shadowlands
+      </Typography>
+    </div>
+  );
+}
---
+++ src/pages/timelineItemComponent.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+// import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+// import HotelIcon from '@material-ui/icons/Hotel';
+// import RepeatIcon from '@material-ui/icons/Repeat';
+// import MenuBook from '@material-ui/icons/MenuBook';
+// import LibraryBooks from '@material-ui/icons/LibraryBooks';
+// import Bathtub from '@material-ui/icons/Bathtub'
+// import SportsEsportsIcon from '@material-ui/icons/SportsEsports'
+// import BurstModeIcon from '@material-ui/icons/BurstMode'
+import {MenuBook, PlayArrow, Hotel, Repeat, LibraryBooks, Bathtub, SportsEsports, BurstMode} from '@material-ui/icons';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  media: {
+    height: 300,
+  },
+}));
+
+
+function chooseIcon(mediaType) {
+  switch(mediaType) {
+    case 'Novel':
+    case 'Illustrated Novel':
+      return <MenuBook />
+    case 'Short Story':
+      return <LibraryBooks />
+    case 'Game':
+      return <SportsEsports />
+    case 'Manga':
+      return <BurstMode />
+    case 'Video':
+      return <PlayArrow />
+    default:
+      return <Bathtub />
+  }
+};
+
+export default function TimelineItemComponent(props) {
+  const classes = useStyles();
+
+
+  return(
+    <TimelineItem>
+      <TimelineOppositeContent>
+        <Typography variant="body2" color="textSecondary">
+          Year {props.year}
+        </Typography>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <TimelineDot color="primary">
+          {chooseIcon(props.type)}
+        </TimelineDot>
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={props.image}
+              title= {props.title + " Cover"}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {props.title}
+              </Typography>
+              <Typography gutterBottom variant="subtitle1" color="textSecondary">
+                {props.type}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+              A journey through an age of myth and legend, a time long before the Horde and the Alliance came to be.
+              This definitive tome of Warcraft history reveals untold stories about the birth of the cosmos, the rise of
+                ancient empires, and the forces that shaped the world of Azeroth and its people.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+            <Button size="small" color="primary" disabled>
+              Amazon
+            </Button>
+          </CardActions>
+        </Card>
+      </TimelineContent>
+    </TimelineItem>
+  );
+};
